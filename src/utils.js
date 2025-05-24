347776260cb7f8a604711c6e91136fc831cceb18
BlackWolf85 <147985619+BlackWolf85@users.noreply.github.com>
2025-05-24 11:05:47 +0500

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -3,3 +3,27 @@ export const getRandomInt = (a = 0, b = 1) => {
   const upper = Math.floor(Math.max(a, b));
   return Math.floor(lower + Math.random() * (upper - lower + 1));
 };
+
+export const sortByDay = (first, second) => {
+  if (first.dateFrom === null && second.dateFrom === null) {
+    return 0;
+  }
+  if (first.dateFrom === null) {
+    return 1;
+  }
+  if (second.dateFrom === null) {
+    return -1;
+  }
+
+  const firstDate = new Date(first.dateFrom).getTime();
+  const secondDate = new Date(second.dateFrom).getTime();
+  return firstDate - secondDate;
+};
+
+export const sortByTime = (first, second) => {
+  const firstDuration = new Date(first.dateTo).getTime() - new Date(first.dateFrom).getTime();
+  const secondDuration = new Date(second.dateTo).getTime() - new Date(second.dateFrom).getTime();
+  return secondDuration - firstDuration;
+};
+
+export const sortByPrice = (first, second) => second.basePrice - first.basePrice;
