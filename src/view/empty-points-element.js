977f2f4c87a576524f26c4cf50ec74ad31a7150d
BlackWolf85 <147985619+BlackWolf85@users.noreply.github.com>
2025-06-07 10:04:30 +0500

    Update empty-points-element.js

--- src/view/empty-points-element.js
+++ src/view/empty-points-element.js
@@ -1,11 +1,26 @@
 import AbstractView from '../framework/view/abstract-view.js';
+import {Filters} from '../constants.js';
 
-const emptyPointTemplate = () => (
-  '<p class="trip-events__msg">Click New Event to create your first point</p>' // Используем разметку из /markup
+const emptyMessages = {
+  [Filters.EVERYTHING]: 'Click New Event to create your first point',
+  [Filters.PAST]: 'There are no past events now',
+  [Filters.PRESENT]: 'There are no present events now',
+  [Filters.FUTURE]: 'There are no future events now',
+};
+
+const createEmptyTemplate = (message) => (
+  `<p class="trip-events__msg">${message}</p>`
 );
 
 export default class EmptyPointElement extends AbstractView {
+  #message = null;
+
+  constructor({messageType = Filters.EVERYTHING} = {}) {
+    super();
+    this.#message = emptyMessages[messageType] || emptyMessages[Filters.EVERYTHING];
+  }
+
   get template() {
-    return emptyPointTemplate();
+    return createEmptyTemplate(this.#message);
   }
 }
