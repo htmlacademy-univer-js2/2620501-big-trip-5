98a1f936529c5b783782c58b149f0532c353029a
BlackWolf85 <147985619+BlackWolf85@users.noreply.github.com>
2025-06-09 21:46:38 +0500

    Update trip-element.js

--- src/view/trip-element.js
+++ src/view/trip-element.js
@@ -1,30 +1,31 @@
 import AbstractView from '../framework/view/abstract-view.js';
+import {encodeHtml} from '../utils.js';
 
-const tripTemplate = (title, date, totalPrice) => (
+const tripTemplate = (title, dates, totalCost) => (
   `<section class="trip-main__trip-info  trip-info">
     <div class="trip-info__main">
-      <h1 class="trip-info__title">${title}</h1>
-      <p class="trip-info__dates">${date}</p>
+      <h1 class="trip-info__title">${encodeHtml(title)}</h1>
+      <p class="trip-info__dates">${encodeHtml(dates)}</p>
     </div>
     <p class="trip-info__cost">
-      Total: €&nbsp;<span class="trip-info__cost-value">${totalPrice}</span>
+      Total: €&nbsp;<span class="trip-info__cost-value">${encodeHtml(totalCost)}</span>
     </p>
   </section>`
 );
 
 export default class TripElement extends AbstractView {
   #title = null;
-  #date = null;
-  #totalPrice = null;
+  #dates = null;
+  #totalCost = null;
 
-  constructor({title, date, totalPrice}) {
+  constructor({title, dates, totalCost}) {
     super();
     this.#title = title;
-    this.#date = date;
-    this.#totalPrice = totalPrice;
+    this.#dates = dates;
+    this.#totalCost = totalCost;
   }
 
   get template() {
-    return tripTemplate(this.#title, this.#date, this.#totalPrice);
+    return tripTemplate(this.#title, this.#dates, this.#totalCost);
   }
 }
