3691849e094254bf213b06181b45acb3fc10683a
BlackWolf85 <147985619+BlackWolf85@users.noreply.github.com>
2025-06-09 21:45:15 +0500

    Update and rename empty-points-element.js to empty-element.js

---
+++ src/view/empty-element.js
@@ -0,0 +1,29 @@
+import AbstractView from '../framework/view/abstract-view.js';
+import {Filters} from '../constants.js';
+import {encodeHtml} from '../utils.js';
+
+const DEFAULT_MESSAGE_TYPE = Filters.EVERYTHING;
+
+const EmptyMessages = {
+  [Filters.EVERYTHING]: 'Click New Event to create your first point',
+  [Filters.PAST]: 'There are no past events now',
+  [Filters.PRESENT]: 'There are no present events now',
+  [Filters.FUTURE]: 'There are no future events now',
+};
+
+const emptyTemplate = (message) => (
+  `<p class="trip-events__msg">${encodeHtml(message)}</p>`
+);
+
+export default class EmptyElement extends AbstractView {
+ #message = '';
+
+  constructor({ messageType = DEFAULT_MESSAGE_TYPE, customMessage = null } = {}) {
+    super();
+    this.#message = customMessage ?? EmptyMessages[messageType] ?? EmptyMessages[DEFAULT_MESSAGE_TYPE];
+  }
+
+  get template() {
+    return createEmptyListTemplate(this.#message);
+  }
+}
